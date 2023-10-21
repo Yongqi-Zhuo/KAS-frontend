8d75f3f96aa3f95079c90db3128ef52573009a7c
Yongqi-Zhuo <Yongqi-Zhuo@users.noreply.github.com>
2023-10-21 19:43:01 +0800

    Better UI.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
-import { Button, Col, Container, Form } from 'react-bootstrap';
-import NavBar from './NavBar';
+import { Breadcrumb, Button, Col, Grid, Input, Layout, Row, Select, Space, Typography } from 'antd';
 import { ExplorerChild, ExplorerPredicate, ExplorerRequest, ExplorerResponse, ExplorerType } from './types';
 
+const styles = {
+  code: {
+    fontFamily: 'monospace',
+    whiteSpace: 'pre-wrap',
+  } as React.CSSProperties,
+};
+
 function App() {
-  const [backendAddress, setBackendAddress] = useState('');
+  const [backendAddress, setBackendAddress] = useState('http://localhost:7070');
   const [explorerType, setExplorerType] = useState<ExplorerType>('search_space');
 
   const [predicates, setPredicates] = useState<ExplorerPredicate[]>([]);
   const [helpMessage, setHelpMessage] = useState<string>('Loading...');
 
   const [state, setState] = useState<string[]>([]);
-  const [exists, setExists] = useState<boolean>(false);
   const [info, setInfo] = useState<string>('Please set the backend address.');
   const [consoleOutput, setConsoleOutput] = useState<string[]>(['KAS Explorer.']);
   const [children, setChildren] = useState<ExplorerChild[]>([]);
@@ -21,7 +26,7 @@ function App() {
       return;
     }
     let result = [...consoleOutput, message];
-    if (result.length > 100) {
+    if (result.length > 5) {
       result.shift();
     }
     setConsoleOutput(result);
@@ -53,12 +58,11 @@ function App() {
       help_message,
     }: ExplorerResponse = await response.json();
     setState(newState);
-    setExists(valid);
     setInfo(info);
     setChildren(children);
     logToConsole(message);
     if (download_url) {
-      window.open(download_url);
+      window.open(`${backendAddress}/${download_url}`, '_blank');
     }
     if (available_predicates) {
       setPredicates(available_predicates);
@@ -78,63 +82,87 @@ function App() {
   };
 
   return (
-    <Container>
-      <Form>
-        <Form.Group controlId="backendAddress">
-          <Form.Label>Backend Address</Form.Label>
-          <Form.Control type="text" placeholder="http://localhost:8080" value={backendAddress} onChange={(e) => {
+    <Layout>
+      <Layout.Header>
+        <Typography.Title level={2} style={{ color: 'white', margin: '15px' }}>KAS Explorer</Typography.Title>
+      </Layout.Header>
+      <Layout.Content style={{ paddingLeft: '50px', paddingRight: '50px', paddingTop: '30px', paddingBottom: '30px' }}>
+        <Space.Compact>
+          <Input value={backendAddress} onChange={(e) => {
             setBackendAddress(e.target.value);
           }} />
-          <Button variant="primary" onClick={async () => {
+          <Button type="primary" onClick={async () => {
             await fetchState(explorerType, state, 'help');
             setConsoleOutput(['KAS Explorer.']);
           }}>Apply</Button>
-        </Form.Group>
-        <Form.Group controlId="explorerType">
-          <Form.Label>Explorer Type</Form.Label>
-          <Form.Control as="select" value={explorerType} onChange={async (e) => {
-            const explorer = e.target.value as ExplorerType;
+          <Select defaultValue="search_space" value={explorerType} onChange={async (value) => {
+            const explorer = value as ExplorerType;
             setExplorerType(explorer);
             await fetchState(explorer, state, 'help');
             setConsoleOutput(['KAS Explorer.']);
           }}>
-            <option value="search_space">Search Space</option>
-            <option value="algorithm">Algorithm-Specific</option>
-          </Form.Control>
-        </Form.Group>
-      </Form>
-      <NavBar state={["Root", ...state]} onBreadcrumbClick={async (index) => {
-        await updateState(state.slice(0, index));
-      }} />
-      <Container>
-        <Col>
-          <h2>Info</h2>
-          <p>{info}</p>
-          <h2>Console</h2>
-          {consoleOutput.map((message, index) => (
-            <p key={index}>{message}</p>
-          ))}
-        </Col>
-        <Col>
-          <h2>Children</h2>
-          {children.map((child, index) => (
-            <Container>
-              <Button key={index} variant="primary" onClick={async () => {
-                await updateState([...state, child.value]);
-              }}>{child.value}</Button>
-              <p>{child.caption}</p>
-            </Container>
-          ))}
-          {/* {predicates.map((predicate, index) => (
-            <Button key={index} variant="primary" onClick={async () => {
-              await handlePredicate(predicate.name, predicate.args);
-            }}>{predicate.name}</Button>
-          ))} */}
-          <h2>Help</h2>
-          <p>{helpMessage}</p>
-        </Col>
-      </Container >
-    </Container>
+            <Select.Option value="search_space">Search Space</Select.Option>
+            <Select.Option value="algorithm">Algorithm-Specific</Select.Option>
+          </Select>
+        </Space.Compact>
+        <Row gutter={[10, 10]}>
+          <Col sm={24} md={8} style={{ paddingRight: '30px' }}>
+            <h2>State</h2>
+            <Breadcrumb items={["Root", ...state].map((value, index) => ({
+              title: value,
+              onClick: async () => {
+                await updateState(state.slice(0, index));
+              },
+            }))} />
+            <h2>Info</h2>
+            <Typography.Text style={styles.code}>{info}</Typography.Text>
+            <h2>Actions</h2>
+            <Row gutter={[10, 10]} align='middle' justify='start' >
+              {predicates.map((predicate, index) => (
+                <Col key={index} md={12} lg={8}>
+                  <Button style={{ width: '100%' }} key={index} type="primary" onClick={async () => {
+                    const args = predicate.additional_args.map((arg, index) => {
+                      return window.prompt(`Enter ${index}-th argument: (${arg})`);
+                    });
+                    if (args.includes(null)) {
+                      return;
+                    }
+                    await handlePredicate(predicate.name, args as string[]);
+                  }}>{predicate.name}</Button>
+                </Col>
+              ))}
+            </Row>
+            <h2>Console</h2>
+            <Col>
+            {consoleOutput.toReversed().map((message, index) => (
+              <Row key={index}>
+                <Typography.Text style={styles.code} key={index}>{message}</Typography.Text>
+              </Row>
+            ))}
+            </Col>
+            <h2>Help</h2>
+            <Typography.Text style={styles.code}>{helpMessage}</Typography.Text>
+          </Col>
+          <Col sm={24} md={16}>
+            <h2>Children</h2>
+            <Row align='middle' gutter={[10, 5]}>
+              {children.map((child, index) => (
+                <Col key={index}>
+                  <Row>
+                    <Button type="primary" onClick={async () => {
+                      await updateState([...state, child.value]);
+                    }}>{child.value}</Button>
+                  </Row>
+                  <Row>
+                    <Typography.Text style={styles.code}>{child.caption}</Typography.Text>
+                  </Row>
+                </Col>
+              ))}
+            </Row>
+          </Col>
+        </Row>
+      </Layout.Content>
+    </Layout>
   );
 }
 
